12bbd31e16fe9bafcd2274aa0f892f42003864b6
thaim <thaim24@gmail.com>
2019-11-15 18:19:45 +0900

    define and use aws config variables

--- components/cloudtrail/main.tf
+++ components/cloudtrail/main.tf
@@ -9,3 +9,9 @@ terraform {
 
   required_version = "= 0.12.13"
 }
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "${var.aws_region}"
+}
--- components/cloudtrail/variables.tf
+++ components/cloudtrail/variables.tf
@@ -3,5 +3,18 @@ variable "aws_profile" {
   default = "default"
 }
 
+variable "aws_access_key" {
+  type = "string"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+}
+
+variable "aws_region" {
+  type = "string"
+  default = "ap-northeast-1"
+}
+
 
 data "aws_caller_identity" "current" {}
