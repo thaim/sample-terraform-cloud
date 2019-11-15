19bab089b176797049998f3343c331b1a98f539c
thaim <thaim24@gmail.com>
2019-11-15 16:45:38 +0900

    update backend as remote

--- components/cloudtrail/main.tf
+++ components/cloudtrail/main.tf
@@ -1,16 +1,11 @@
 terraform {
-  backend "s3" {
-    // 以下のパラメタは backend.tfvarsで指定する
-    // bucket = "tfstate-bucket-name"
-    // key = "tfstate-key-name"
+  backend "remote" {
+    organization = "thaim"
+  }
+
+  workspaces {
+    name = "sample-tfc-cloudtrail"
   }
 
   required_version = "= 0.12.13"
 }
-
-provider "aws" {
-  region  = "ap-northeast-1"
-  profile = var.aws_profile
-  # profile = "spacely"
-}
-
