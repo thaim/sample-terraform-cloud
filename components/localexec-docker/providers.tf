6ea88162967ee9b62c925c13d3535356cf715f12
thaim <thaim24@gmail.com>
2021-07-04 22:16:20 +0000

    add docker provider

--- components/localexec-docker/providers.tf
+++ components/localexec-docker/providers.tf
@@ -5,8 +5,23 @@ terraform {
       name = "local-exec"
     }
   }
+
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.13.0"
+    }
+  }
 }
 
 provider "aws" {
   region = "ap-northeast-1"
+}
+
+provider "docker" {
+  registry_auth {
+    address  = "${data.aws_caller_identity.current.account_id}.dkr.ecr.ap-northeast-1.amazonaws.com"
+    username = data.aws_ecr_authorization_token.token.user_name
+    password = data.aws_ecr_authorization_token.token.password
+  }
 }
